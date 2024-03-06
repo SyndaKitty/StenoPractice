cfb4af57a524d5efa4400f687ad8335199d10d34
SyndaKai <SyndaKitty@gmail.com>
2024-03-05 23:19:03 -0500

    Separate practice into module

--- main.lua
+++ main.lua
@@ -1,222 +1,27 @@
-local font
-local font_alt
-local test_words
-local input_buffer = ""
-local word_index = 1
-local word_spacing = 40
-local line_spacing = 10
-local time_until_delete = -1
-local lshift_count = 0
-local focused = true
-local done = false
-local time_elapsed = 0.01
-local words_typed = 0
-local mistakes = 0
-local started = false
-local filename = "test"
-local sliding_x = 0
-local sliding_x2 = 0
-local tps = 144
-local spt = 1 / tps
-local accumulator = 0
+local practice = require "practice"
 
-local invalid_color = {1, .5, .5, 1}
-local correct_color = {.6, 1, .7, 1}
-local next_color = { .5, .5, .5, 1}
-local current_color = {.9, .9, .9, 1}
-local done_color = { .5, .5, .5, 1}
+local current_scene = practice
 
 function love.load()
-    word_index = 1
-    time_until_delete = -1
-    time_elapsed = .01
-    words_typed = 0
-    input_buffer = ""
-    mistakes = 0
-    done = false
-    started = false
-    sliding_x = 0
-    sliding_x2 = -2
-
-    font = love.graphics.newFont("Hack-Regular.ttf", 48, "normal", 2)
-    font_alt = love.graphics.newFont("Hack-Regular.ttf", 24, "normal", 2)
-    local test_text = love.filesystem.read(filename .. ".txt")
-    test_words = string_split(test_text)
-    shuffle_list(test_words)
-    test_words[#test_words+1] = " "
+    current_scene:load()
 end
 
 function love.update(dt)
-    if done then
-        accumulator = accumulator + dt
-        while accumulator > spt do
-            sliding_x2 = sliding_x2 * 1.08
-            accumulator = accumulator - spt
-        end
-        return
-    end
-    if not focused or not started then return end
-    time_elapsed = time_elapsed + dt
-    if time_until_delete > 0 then
-        time_until_delete = time_until_delete - dt
-        if time_until_delete <= 0 then
-            input_buffer = ""
-        end
-    end
-    
-    accumulator = accumulator + dt
-    while accumulator > spt do
-        sliding_x = sliding_x * .95
-        accumulator = accumulator - spt
-    end
-end
-
-function wpm()
-    return format_number(words_typed / (time_elapsed / 60))
-end
-
-function accuracy()
-    local accuracy = 0
-    if words_typed + mistakes > 0 then
-        accuracy = 100 - (mistakes / (words_typed + mistakes)) * 100
-    end
-    return format_number(accuracy)
+    current_scene:update(dt)
 end
 
 function love.draw()
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.print("WPM: " .. wpm(), font_alt, word_spacing, line_spacing)
-
-    local accuracy_str = "Accuracy: " .. accuracy() .. "%"
-    local acc_x = love.graphics.getWidth() - font_alt:getWidth(accuracy_str) - word_spacing
-    love.graphics.print(accuracy_str, font_alt, acc_x)
-
-    target_word = test_words[word_index]
-
-    local center_x = love.graphics.getWidth() / 2 - 60
-    local target_length = font:getWidth(target_word)
-    local word_x = center_x + sliding_x + sliding_x2
-
-    local center_y = love.graphics.getHeight() / 2
-    local target_height = font:getHeight()
-    local word_y = center_y - target_height / 2
-
-    love.graphics.print({current_color, target_word}, font, word_x, word_y)
-
-    -- Draw previous words
-    local cursor_x = word_x
-    local cursor_y = word_y
-    local i = word_index - 1
-    while i > 0 do
-        local word = test_words[i]
-        cursor_x = cursor_x - word_spacing - font:getWidth(word)
-        love.graphics.print({done_color, word}, font, cursor_x, cursor_y)
-        love.graphics.print({correct_color, word}, font, cursor_x, cursor_y + line_spacing + target_height)
-        i = i - 1
-    end
-
-    -- Draw next words
-    cursor_x = word_x + target_length + word_spacing
-    cursor_y = word_y
-    i = word_index + 1
-    while i < #test_words + 1 do
-        local word = test_words[i]
-        love.graphics.print({next_color, word}, font, cursor_x, cursor_y)
-        cursor_x = cursor_x + word_spacing + font:getWidth(word)
-        i = i + 1
-    end
-
-    -- Draw input buffer
-    cursor_y = word_y + font:getHeight() + line_spacing
-    cursor_x = word_x
-    for i = 1, #input_buffer do
-        local c = string.sub(input_buffer, i, i)
-        if c == string.sub(target_word, i, i) then
-            love.graphics.print({correct_color, c}, font, cursor_x, cursor_y)
-        else
-            love.graphics.print({invalid_color, c}, font, cursor_x, cursor_y)
-        end
-        cursor_x = cursor_x + font:getWidth(c)
-    end
+    current_scene:draw()
 end
 
 function love.keypressed(key)
-    if key == "lshift" then
-        lshift_count = lshift_count + 1
-        if lshift_count == 3 then
-            love.load()
-            lshift_count = 0
-        end
-    else
-        started = true
-        lshift_count = 0
-    end
-    if key == "backspace" then
-        input_buffer = string.sub(input_buffer, 1, #input_buffer - 1)
-    end
+    current_scene:keypressed(key)
 end
 
 function love.textinput(text)
-    if done or text == " " then return end
-    started = true
-    input_buffer = input_buffer .. text
-    if input_buffer == test_words[word_index] then
-        sliding_x = sliding_x + font:getWidth(test_words[word_index]) + word_spacing
-        word_index = word_index + 1
-        input_buffer = ""
-        words_typed = words_typed + 1
-
-        if word_index == #test_words then
-            save_score(filename, wpm(), accuracy())
-            done = true
-        end
-    elseif partial_match(input_buffer, test_words[word_index]) then
-        -- Ignore
-    elseif time_until_delete >= 0 then
-        time_until_delete = .4
-    else
-        time_until_delete = .4
-        mistakes = mistakes + 1
-    end
+    current_scene:textinput(text)
 end
 
 function love.focus(f)
-    focused = f
-end
-
-function string_split(str)
-    t = {}
-    for k in string.gmatch(str, "[^%s]+") do
-        t[#t+1] = k
-    end
-    return t
-end
-
-function shuffle_list(list)
-    for i = #list, 2, -1 do
-        local j = love.math.random(i)
-        list[i], list[j] = list[j], list[i]
-    end
-end
-
-function partial_match(str1, str2)
-    for i = 1,math.min(#str1, #str2) do
-        if string.sub(str1, i, i) ~= string.sub(str2, i, i) then
-            return false
-        end
-    end
-    return true
-end
-
-function format_number(num)
-    return string.format("%.1f", num)
-end
-
-function save_score(file, wpm, accuracy)
-    local score_filename = file .. "_score.txt"
-    local score_row = os.date("%m/%d %H:%M:%S ") .. wpm .. " " .. accuracy .. "\n"
-    
-    local f = love.filesystem.newFile(score_filename, "a")
-    f:write(score_row)
-    f:close()
+    current_scene:focus(f)
 end
---
+++ practice.lua
@@ -0,0 +1,224 @@
+local practice = {
+    font = love.graphics.newFont("Hack-Regular.ttf", 48, "normal", 2),
+    font_alt = love.graphics.newFont("Hack-Regular.ttf", 24, "normal", 2),
+    test_words = {},
+    input_buffer = "",
+    word_index = 1,
+    word_spacing = 40,
+    line_spacing = 10,
+    time_until_delete = -1,
+    lshift_count = 0,
+    focused = true,
+    done = false,
+    time_elapsed = 0.01,
+    words_typed = 0,
+    mistakes = 0,
+    started = false,
+    filename = "test",
+    left_shift = 0,
+    right_shift = 0,
+    tps = 144,
+    spt = 1 / 144,
+    accumulator = 0,
+    invalid_color = {1, .5, .5, 1},
+    correct_color = {.6, 1, .7, 1},
+    next_color = { .5, .5, .5, 1},
+    current_color = {.9, .9, .9, 1},
+    done_color = { .5, .5, .5, 1}
+}
+
+
+function practice:load()
+    self.word_index = 1
+    self.time_until_delete = -1
+    self.time_elapsed = .01
+    self.words_typed = 0
+    self.input_buffer = ""
+    self.mistakes = 0
+    self.done = false
+    self.started = false
+    self.left_shift = 0
+    self.right_shift = -2
+
+    local test_text = love.filesystem.read(self.filename .. ".txt")
+    self.test_words = self:string_split(test_text)
+    self:shuffle_list(self.test_words)
+    self.test_words[#self.test_words+1] = " "
+end
+
+function practice:update(dt)
+    if self.done then
+        self.accumulator = self.accumulator + dt
+        while self.accumulator > self.spt do
+            self.right_shift = self.right_shift * 1.08
+            self.accumulator = self.accumulator - self.spt
+        end
+        return
+    end
+    if not self.focused or not self.started then return end
+    self.time_elapsed = self.time_elapsed + dt
+    if self.time_until_delete > 0 then
+        self.time_until_delete = self.time_until_delete - dt
+        if self.time_until_delete <= 0 then
+            self.input_buffer = ""
+        end
+    end
+    
+    self.accumulator = self.accumulator + dt
+    while self.accumulator > self.spt do
+        self.left_shift = self.left_shift * .95
+        self.accumulator = self.accumulator - self.spt
+    end
+end
+
+function practice:wpm()
+    return self:format_number(self.words_typed / (self.time_elapsed / 60))
+end
+
+function practice:accuracy()
+    local accuracy = 0
+    if self.words_typed + self.mistakes > 0 then
+        accuracy = 100 - (self.mistakes / (self.words_typed + self.mistakes)) * 100
+    end
+    return self:format_number(accuracy)
+end
+
+function practice:draw()
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print("WPM: " .. self:wpm(), self.font_alt, self.word_spacing, self.line_spacing)
+
+    local accuracy_str = "Accuracy: " .. self:accuracy() .. "%"
+    local acc_x = love.graphics.getWidth() - self.font_alt:getWidth(accuracy_str) - self.word_spacing
+    love.graphics.print(accuracy_str, self.font_alt, acc_x)
+
+    self.target_word = self.test_words[self.word_index]
+
+    local center_x = love.graphics.getWidth() / 2 - 60
+    local target_length = self.font:getWidth(self.target_word)
+    local word_x = center_x + self.left_shift + self.right_shift
+
+    local center_y = love.graphics.getHeight() / 2
+    local target_height = self.font:getHeight()
+    local word_y = center_y - target_height / 2
+
+    love.graphics.print({self.current_color, self.target_word}, self.font, word_x, word_y)
+
+    -- Draw previous words
+    local cursor_x = word_x
+    local cursor_y = word_y
+    local i = self.word_index - 1
+    while i > 0 do
+        local word = self.test_words[i]
+        cursor_x = cursor_x - self.word_spacing - self.font:getWidth(word)
+        love.graphics.print({self.done_color, word}, self.font, cursor_x, cursor_y)
+        love.graphics.print({self.correct_color, word}, self.font, cursor_x, cursor_y + self.line_spacing + target_height)
+        i = i - 1
+    end
+
+    -- Draw next words
+    cursor_x = word_x + target_length + self.word_spacing
+    cursor_y = word_y
+    i = self.word_index + 1
+    while i < #self.test_words + 1 do
+        local word = self.test_words[i]
+        love.graphics.print({self.next_color, word}, self.font, cursor_x, cursor_y)
+        cursor_x = cursor_x + self.word_spacing + self.font:getWidth(word)
+        i = i + 1
+    end
+
+    -- Draw input buffer
+    cursor_y = word_y + self.font:getHeight() + self.line_spacing
+    cursor_x = word_x
+    for i = 1, #self.input_buffer do
+        local c = string.sub(self.input_buffer, i, i)
+        if c == string.sub(self.target_word, i, i) then
+            love.graphics.print({self.correct_color, c}, self.font, cursor_x, cursor_y)
+        else
+            love.graphics.print({self.invalid_color, c}, self.font, cursor_x, cursor_y)
+        end
+        cursor_x = cursor_x + self.font:getWidth(c)
+    end
+end
+
+function practice:keypressed(key)
+    if key == "lshift" then
+        self.lshift_count = self.lshift_count + 1
+        if self.lshift_count == 3 then
+            love.load()
+            self.lshift_count = 0
+        end
+    else
+        self.started = true
+        self.lshift_count = 0
+    end
+    if key == "backspace" then
+        self.input_buffer = string.sub(self.input_buffer, 1, #self.input_buffer - 1)
+    end
+end
+
+function practice:textinput(text)
+    if self.done or text == " " then return end
+    self.started = true
+    self.input_buffer = self.input_buffer .. text
+    if self.input_buffer == self.test_words[self.word_index] then
+        self.left_shift = self.left_shift + self.font:getWidth(self.test_words[self.word_index]) + self.word_spacing
+        self.word_index = self.word_index + 1
+        self.input_buffer = ""
+        self.words_typed = self.words_typed + 1
+
+        if self.word_index == #self.test_words then
+            self:save_score(self.filename, self:wpm(), self:accuracy())
+            self.done = true
+        end
+    elseif self:partial_match(self.input_buffer, self.test_words[self.word_index]) then
+        -- Ignore
+    elseif self.time_until_delete >= 0 then
+        self.time_until_delete = .4
+    else
+        self.time_until_delete = .4
+        self.mistakes = self.mistakes + 1
+    end
+end
+
+function practice:focus(f)
+    self.focused = f
+end
+
+function practice:string_split(str)
+    local t = {}
+    for k in string.gmatch(str, "[^%s]+") do
+        t[#t+1] = k
+    end
+    return t
+end
+
+function practice:shuffle_list(list)
+    for i = #list, 2, -1 do
+        local j = love.math.random(i)
+        list[i], list[j] = list[j], list[i]
+    end
+end
+
+function practice:partial_match(str1, str2)
+    for i = 1,math.min(#str1, #str2) do
+        if string.sub(str1, i, i) ~= string.sub(str2, i, i) then
+            return false
+        end
+    end
+    return true
+end
+
+function practice:format_number(num)
+    return string.format("%.1f", num)
+end
+
+function practice:save_score(file, wpm, accuracy)
+    local score_filename = file .. "_score.txt"
+    local score_row = os.date("%m/%d %H:%M:%S ") .. wpm .. " " .. accuracy .. "\n"
+    
+    local f = love.filesystem.newFile(score_filename, "a")
+    f:write(score_row)
+    f:close()
+end
+
+return practice
